37ced08d5939e0ebacc5e6a40ea9fcb1a922fff1
JackYap123 <B2100346@helplive.edu.my>
2024-01-30 08:15:51 +0800

    remove the space

--- Assignment/User_Edit_Profile.php
+++ Assignment/User_Edit_Profile.php
@@ -1,7 +1,18 @@
+<?php
+    session_start();
+
+    include_once("php/config.php");
+    if(!isset($_SESSION['valid']))
+    {
+        header("Location:Login_Page_User.php");
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Login_Page_User.css">
     <title>Document</title>
@@ -20,21 +31,52 @@
 
     <div class="container">
         <div class="box form-box">
+            <?php
+                if($isset($_POST['submit']))
+                {
+                    $username = $_POST['username'];
+                    $email = $_POST['email'];
+                    $age = $_POST['age'];
+                    
+                    $id = $_SESSION['id'];
+
+                    $edit_query = mysqli_query($con,"UPDATE users SET Username = '$username',Email = '$email',Age=$age WHERE Id='$id'") or die("error occures");
+                    if($edit_query)
+                    {
+                        echo "<div class='message'>
+                        <p>Profile Update!!!</p>
+                      </div>,<br>"  ;
+    
+                        echo"<a href='Home.php'><button class='btn'>Login in now</button>";
+                    }
+                    else{
+                        $id = $_SESSION['id'];
+                        $query = mysqli_query($con,"SELECT * FROM users WHERE Id = $id");
+
+                        while($result = mysqli_fetch_assoc($query))
+                        {
+                            $res_Uname = $result('UserName');
+                            $res_Email = $result('Email');
+                            $res_Age = $result('age');
+                        }
+                    }
+                
+            ?>
             <header>Change Profile</header>
             <form action="" method="post">
                 <div class="field input">
                     <label for="username">Username</label>
-                    <input type="text" name="username" id="username" autocomplete="off" required>
+                    <input type="text" name="username" id="username" value="<?php echo $res_Uname;?>" autocomplete="off" required>
                 </div>
 
                 <div class="field input">
                     <label for="email">Email</label>
-                    <input type="text" name="email" id="email" autocomplete="off" required>
+                    <input type="text" name="email" id="email" value="<?php echo $res_Email;?>"autocomplete="off" required>
                 </div>
                 
                 <div class="field input">
                     <label for="age">Age</label>
-                    <input type="number" name="age" id="age" autocomplete="off" required>
+                    <input type="number" name="age" id="age" value="<?php echo $res_Age;?>" autocomplete="off" required>
                 </div>
     
                 <div class="field">
@@ -42,6 +84,7 @@
                 </div>
             </form>
         </div>
+        <?php } ?>
     </div>
    
 </body>
