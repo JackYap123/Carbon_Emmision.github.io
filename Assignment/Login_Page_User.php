a9829074f80b8eccf1f1cf8df31cefe1e7959e2f
JackYap123 <B2100346@helplive.edu.my>
2024-03-18 10:27:59 +0800

    delete the old testing part an the main page change to the index.php

--- Assignment/Login_Page_User.php
+++ Assignment/Login_Page_User.php
@@ -36,7 +36,7 @@
                     echo "<a href='Login_Page_User.php'><button class='btn'>Go Back</button>";
                 }
                 if (isset($_SESSION['valid'])) {
-                    header("Location: Main_Page.php");
+                    header("Location: index.php");
                 }
             } else {
             ?>
