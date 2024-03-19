76e95a02f7498539a000d9ebc7096e03593820dc
JackYap123 <B2100346@helplive.edu.my>
2024-03-19 14:20:35 +0800

    Changes

--- Assignment/Login_Page_User.php
+++ Assignment/Login_Page_User.php
@@ -58,6 +58,8 @@
                         Don't have account? <a href="register.php">Sign Up New Account</a>
                     </div>
                 </form>
+
+                <a href="forgot_Password.php">Forgot Password?</a>
         </div>
     <?php } ?>
     </div>
