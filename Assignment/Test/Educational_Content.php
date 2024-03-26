f6c2f331763c3a3b08a1865b747661c430e12794
UselessShyt <yowinchen2@gmail.com>
2024-03-26 17:02:56 +0800

    Video imported

--- Assignment/Test/Educational_Content.php
+++ Assignment/Test/Educational_Content.php
@@ -8,24 +8,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Educational Content</title>
-    <link rel="stylesheet" href="CSS/Header.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="CSS/Footer.css">
     <link rel="stylesheet" href="CSS/Educational_Content.css">
 </head>
 <body>
     <div style="height: 5em;"></div>
     <div class="container1">
-        <h1 class="title">Educational Content</h1>
+        <h1 id="title1">Educational Content</h1>
         <p class="content">Understand how our action impact the enviroment</p>
         <button>Learn</button>
     </div>
     <div class="container2">
-        <h2>Educational Video</h2>
-        <iframe width="1080" height="607.5" src="https://www.youtube.com/embed/8q7_aV8eLUE?si=_3yFmrfeZOxX0JtN" frameborder="0" allowfullscreen></iframe>
+        <h2 id="title2">Educational Video</h2>
+        <div class="video-container">
+            <iframe src="https://www.youtube.com/embed/8q7_aV8eLUE?si=_3yFmrfeZOxX0JtN" frameborder="0" allowfullscreen></iframe>
+            <iframe src="https://www.youtube.com/embed/fStmxIfwXeI?si=HgyqEqTNABSmyKfM" frameborder="0" allowfullscreen></iframe>
+        </div>
+        
     </div>
+    
 </body>
 </html>
 
