e8b26329b077c7e7de3fe5e564a74872324dc25b
JackYap123 <B2100346@helplive.edu.my>
2024-03-30 09:01:24 +0800

    delete the db_connection, we are using config.php

--- Assignment/Eco_Events_Calendar.php
+++ Assignment/Eco_Events_Calendar.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Eco Events Calendar</title>
-    <link rel="stylesheet" href="EcoEventsCalendar.css">
+    <link rel="stylesheet" href="css/EcoEventsCalendar.css">
 </head>
 <body>
     <div class="container">
