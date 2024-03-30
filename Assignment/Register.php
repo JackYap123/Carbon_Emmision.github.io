72101157c8a695ad70152408f958cc8f59e9bc64
JackYap123 <B2100346@helplive.edu.my>
2024-03-30 09:21:01 +0800

    change

--- Assignment/Register.php
+++ Assignment/Register.php
@@ -1,6 +1,6 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,74 +8,84 @@
     <link rel="javascript" href="js/register_rules.js">
     <title>Register</title>
 </head>
+
 <body>
     <div class="container">
-        <div class="box form-box" >
-        <?php
-// PHP code for handling form submission
-    include_once("php/config.php");
-    if(isset($_POST['submit']))
-    {
-        $username = $_POST['username'];
-        $email = $_POST['email'];
-        $age = $_POST['age'];
-        $password = $_POST['password'];
+        <div class="box form-box">
+            <?php
+            // PHP code for handling form submission
+            include_once("php/config.php");
+            if (isset($_POST['submit'])) {
+                $username = $_POST['username'];
+                $email = $_POST['email'];
+                $age = $_POST['age'];
+                $password = $_POST['password'];
+                $food = $_POST['food'];
+                $commuting_method = $_POST['commuting-method']; // Corrected variable name
+                $energy = $_POST['energy'];
 
-        $verify_query = mysqli_query($con,"SELECT Email FROM users WHERE Email = '$email'");
+                $verify_query = mysqli_query($con, "SELECT Email FROM users WHERE Email = '$email'");
 
-        if(mysqli_num_rows($verify_query)!=0)
-        {
-            echo "<div class='message'>
+                if (mysqli_num_rows($verify_query) != 0) {
+                    echo "<div class='message'>
                     <p> This Email is used, Try another One Please!</p>
                   </div>,<br>";
 
-            echo"<a href='javascript:self.history.back()'><button class='btn'>Go Back</button>";
-        }
-        else{
-            mysqli_query($con,"INSERT INTO users(Username,Email,Age,PASSWORD) VALUES('$username','$email','$age','$password')") or die("Error Occuped");
-        
-            echo "<div class='message'>
+                    echo "<a href='javascript:self.history.back()'><button class='btn'>Go Back</button>";
+                } else {
+                    $insert_query = mysqli_query($con, "INSERT INTO users (Username, Email, Age, Password, food, commuting_method, energy) VALUES ('$username', '$email', '$age', '$password', '$food', '$commuting_method', '$energy')") or die("Error Occupied");
+
+                    echo "<div class='message'>
                     <p> Resgitration Successfully</p>
-                  </div>,<br>"  ;
+                  </div>,<br>";
 
-            echo"<a href='Login_Page_User.php'><button class='btn'>Login in now</button>";
-        }
-    }
-    
-    else{
-?>
-            <header>Sign Up</header>
-            <form action="" method="post" id="register_form">
-                <div class="field input">
-                    <label for="username">Username</label>
-                    <input type="text" name="username" id="username" autocomplete="new-password" required >
-                </div>
+                    echo "<a href='Login_Page_User.php'><button class='btn'>Login in now</button>";
+                }
+            } else {
+            ?>
+                <header>Sign Up</header>
+                <form action="" method="post" id="register_form">
+                    <div class="field input">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" id="username" autocomplete="new-password" required>
+                    </div>
 
-                <div class="field input">
-                    <label for="email">Email</label>
-                    <input type="text" name="email" id="email" autocomplete="new-password" required>
-                </div>
-                
-                <div class="field input">
-                    <label for="age">Age</label>
-                    <input type="number" name="age" id="age" autocomplete="new-password" required>
-                </div>
+                    <div class="field input">
+                        <label for="email">Email</label>
+                        <input type="text" name="email" id="email" autocomplete="new-password" required>
+                    </div>
 
-                <div class="field input">
-                    <label for="password">Password</label>
-                    <input type="password" name="password" id="password" autocomplete="new-password" required>
-                </div>
+                    <div class="field input">
+                        <label for="age">Age</label>
+                        <input type="number" name="age" id="age" autocomplete="new-password" required>
+                    </div>
 
-                <div class="field">
-                    <input type="submit" name="submit" value="Login" class="btn" required>
-                </div>
-                <div class="links">
-                    Already a member<a href="Login_Page_User.php"> Sign In</a>
-                </div>
-            </form>
+                    <div class="field input">
+                        <label for="commuting-method">Commuting-Method</label>
+                        <input type="text" name="commuting-method" id="commuting-method" autocomplete="new-password" required>
+                    </div>
+
+                    <div class="field input">
+                        <label for="energy">Energy</label>
+                        <input type="text" name="energy" id="energy" autocomplete="new-password" required>
+                    </div>
+
+                    <div class="field input">
+                        <label for="food">Food</label>
+                        <input type="text" name="food" id="food" autocomplete="new-password" required>
+                    </div>
+
+                    <div class="field">
+                        <input type="submit" name="submit" value="Login" class="btn" required>
+                    </div>
+                    <div class="links">
+                        Already a member<a href="Login_Page_User.php"> Sign In</a>
+                    </div>
+                </form>
         </div>
-        <?php } ?>
+    <?php } ?>
     </div>
-    
+
 </body>
+
 </html>
--- Assignment/User_Edit_Profile.php
+++ Assignment/User_Edit_Profile.php
@@ -1,11 +1,10 @@
 <?php
-    session_start();
+session_start();
 
-    include_once("php/config.php");
-    if(!isset($_SESSION['valid']))
-    {
-        header("Location:Login_Page_User.php");
-    }
+include_once("php/config.php");
+if (!isset($_SESSION['valid'])) {
+    header("Location:Login_Page_User.php");
+}
 
 ?>
 <!DOCTYPE html>
@@ -34,71 +33,69 @@
     <div class="container">
         <div class="box form-box">
             <?php
-                if(isset($_POST['submit'])){
-                    $username = $_POST['username'];
-                    $email = $_POST['email'];
-                    $age = $_POST['age'];
+            if (isset($_POST['submit'])) {
+                $username = $_POST['username'];
+                $email = $_POST['email'];
+                $age = $_POST['age'];
 
-                    $id = $_SESSION['id'];
+                $id = $_SESSION['id'];
 
-                    $edit_query = mysqli_query($con,"UPDATE users SET UserName='$username',Email='$email',Age='$age' WHERE Id = $id") or die("error occures");
-                
-                    if($edit_query){
-                        echo "<div class='message'>
+                $edit_query = mysqli_query($con, "UPDATE users SET UserName='$username',Email='$email',Age='$age' WHERE Id = $id") or die("error occures");
+
+                if ($edit_query) {
+                    echo "<div class='message'>
                     <p>Profile Updated!</p>
                   </div> <br>";
 
-                    echo"<a href='Main_Page.php'><button class='btn'>Login in now</button>";
-                    }
-                }else{
-                    $id = $_SESSION['user_Id'];
-                    $query = mysqli_query($con,"SELECT * FROM users WHERE user_Id=$id");
-                    
-                    while($result = mysqli_fetch_assoc($query)){
-                        $res_Uname = $result['UserName'];
-                        $res_Email = $result['Email'];
-                        $res_Age = $result['Age'];
-                    }
+                    echo "<a href='Main_Page.php'><button class='btn'>Login in now</button>";
+                }
+            } else {
+                $id = $_SESSION['user_Id'];
+                $query = mysqli_query($con, "SELECT * FROM users WHERE user_Id=$id");
+
+                while ($result = mysqli_fetch_assoc($query)) {
+                    $res_Uname = $result['UserName'];
+                    $res_Email = $result['Email'];
+                    $res_Age = $result['Age'];
+                }
             ?>
-            <header>Change Profile</header>
-            <form action="" method="post">
-                <div class="field input">
-                    <label for="username">Username</label>
-                    <input type="text" name="username" id="username" value="<?php echo $res_Uname;?>" autocomplete="off"
-                        required>
-                </div>
+                <header>Change Profile</header>
+                <form action="" method="post">
+                    <div class="field input">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" id="username" value="<?php echo $res_Uname; ?>" autocomplete="off" required>
+                    </div>
 
-                <div class="field input">
-                    <label for="email">Email</label>
-                    <input type="text" name="email" id="email" value="<?php echo $res_Email;?>" autocomplete="off"
-                        required>
-                </div>
+                    <div class="field input">
+                        <label for="email">Email</label>
+                        <input type="text" name="email" id="email" value="<?php echo $res_Email; ?>" autocomplete="off" required>
+                    </div>
 
-                <div class="field input">
-                    <label for="age">Age</label>
-                    <input type="number" name="age" id="age" value="<?php echo $res_Age;?>" autocomplete="off" required>
-                </div>
-                <div class="field input">
-                    <label for="email">Email</label>
-                    <input type="text" name="email" id="email" autocomplete="new-password" required>
-                </div>
-                
-                <div class="field input">
-                    <label for="age">Age</label>
-                    <input type="number" name="age" id="age" autocomplete="new-password" required>
-                </div>
+                    <div class="field input">
+                        <label for="age">Age</label>
+                        <input type="number" name="age" id="age" value="<?php echo $res_Age; ?>" autocomplete="off" required>
+                    </div>
+                    <div class="field input">
+                        <label for="commuting-method">Commuting-Method</label>
+                        <input type="text" name="commuting-method" id="commuting-method" autocomplete="new-password" required>
+                    </div>
 
-                <div class="field input">
-                    <label for="password">Password</label>
-                    <input type="password" name="password" id="password" autocomplete="new-password" required>
-                </div>
+                    <div class="field input">
+                        <label for="energy">Energy</label>
+                        <input type="text" name="energy" id="energy" autocomplete="new-password" required>
+                    </div>
 
-                <div class="field">
-                    <input type="submit" name="submit" value="Update" class="btn" required>
-                </div>
-            </form>
+                    <div class="field input">
+                        <label for="food">Food</label>
+                        <input type="text" name="food" id="food" autocomplete="new-password" required>
+                    </div>
+
+                    <div class="field">
+                        <input type="submit" name="submit" value="Update" class="btn" required>
+                    </div>
+                </form>
         </div>
-        <?php } ?>
+    <?php } ?>
     </div>
 </body>
 
