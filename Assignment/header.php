bf55e0bf667138b4e54e8cb03e5415d1f6562cce
JackYap123 <B2100346@helplive.edu.my>
2024-04-01 07:16:40 +0800

    changes

--- Assignment/header.php
+++ Assignment/header.php
@@ -81,6 +81,8 @@ $con->close();
                 <a href="Educational_Content.php" class="navi">Learn</a>
                 <a href="recommendations.php" class="navi">Recommendation</a>
                 <a href="Eco_Events_Calendar.php" class="navi">Event Calander</a>
+                <a href="feedback.php" class="navi">Feedback</a>
+
 
                 <span class="notification-icon">
                     <i class="fas fa-bell"></i>
